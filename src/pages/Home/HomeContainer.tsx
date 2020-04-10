fcaf3aa2dd2ba8ef60a87e1cf1b8b8d4db9f237a
Pakhoalot <pakholeung37@gmail.com>
2020-04-11 00:11:37 +0800

    lint

--- src/pages/Home/HomeContainer.tsx
+++ src/pages/Home/HomeContainer.tsx
@@ -1,8 +1,6 @@
 import React from "react";
 import Home from "./Home";
 
-const a = 3;
-
 export default function HomeContainer() {
   return <Home></Home>;
 }
--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,4 +1,3 @@
 import homeRoutes from "./home";
 
-const a = 3;
 export default [...homeRoutes];
