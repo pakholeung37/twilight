d2ae3b3251043191d0d42e4dbca4bf686d20a80a
Pakhoalot <pakholeung37@gmail.com>
2020-04-15 00:21:38 +0800

    这才写个header

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,5 +1,10 @@
 import React from "react";
 import { Button } from "antd";
+import {
+  EyeOutlined,
+  SaveOutlined,
+  CloudUploadOutlined
+} from "@ant-design/icons";
 import Logo from "./Logo";
 
 export default function Header() {
@@ -9,13 +14,15 @@ export default function Header() {
         <Logo />
       </div>
       <div className="header__button-group">
-        <Button type="link" ghost>
+        <Button type="link" icon={<EyeOutlined />}>
           预览
         </Button>
-        <Button type="link" ghost>
+        <Button type="link" icon={<SaveOutlined />}>
           保存
         </Button>
-        <Button type="primary">发布</Button>
+        <Button type="primary" icon={<CloudUploadOutlined />}>
+          发布
+        </Button>
       </div>
     </div>
   );
