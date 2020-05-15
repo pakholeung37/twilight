543736765d87fb4f68378f4df3e55533707be8bb
root <root@DESKTOP-JHIG4OC.localdomain>
2020-05-15 18:30:46 +0800

    good!

--- src/library/compiler/compiler.ts
+++ src/library/compiler/compiler.ts
@@ -2,10 +2,18 @@ export interface NodeSchema {
   type: string;
   children?: string;
   group?: string;
+  enter?: (node: Node, parent: Node | null) => void;
+  exit?: (node: Node, parent: Node | null) => void;
 }
 export interface Schema {
-  nodes: { [key: string]: NodeSchema };
-  rules: ((node: Node) => void)[];
+  nodes: { [key: string]: NodeSchema | undefined };
+  rules?: ((node: Node) => void)[];
+}
+
+export interface Node {
+  type: string;
+  props: any[];
+  children: Node[];
 }
 
 const ArticleList: NodeSchema = {
@@ -16,6 +24,16 @@ const ArticleList: NodeSchema = {
 const Article: NodeSchema = {
   type: "Article",
   group: "atom",
+};
+
+const Swiper: NodeSchema = {
+  type: "Swiper",
+  children: "Slide*",
+};
+
+const Slide: NodeSchema = {
+  type: "Slide",
+  children: "container+",
 };
 
 function createSchema(schemas: NodeSchema[]) {
@@ -46,5 +64,29 @@ export default class Compiler {
 
   schemaCheck() {
     /* TODO */
+  }
+
+  traverser(root: Node, schema: Schema) {
+    function traverse(node: Node, parent: Node | null) {
+      const nodeType = node.type;
+      const nodeSchema = schema.nodes[nodeType];
+      if (!nodeSchema)
+        throw TypeError(
+          `[compiler] nodeType ${node.type} is not on the schema`
+        );
+      if (nodeSchema.enter) {
+        nodeSchema.enter(node, parent);
+      }
+
+      if (node.children) {
+        node.children.map(child => traverse(child, node));
+      }
+
+      if (nodeSchema.exit) {
+        nodeSchema.exit(node, parent);
+      }
+    }
+
+    traverse(root, null);
   }
 }
