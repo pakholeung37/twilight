c53bf5bb3269f9eeec6db133b790abbb38224997
Pakho Leung <pakholeung37@gmail.com>
2020-10-20 00:22:31 +0800

    add Tree component

--- src/components/tree/Tree.tsx
+++ src/components/tree/Tree.tsx
@@ -1,7 +1,60 @@
-import React from "react"
+import React, { useRef, useState } from "react"
+import { Box, Text, Button } from "@chakra-ui/core"
+import { BsFillCaretRightFill } from "react-icons/bs"
+import { useClickAway } from "ahooks"
 
-const Tree: React.FC<{ arg1: boolean }> = ({ arg1 }) => {
-  return <div>tree</div>
+export type TreeNode = {
+  title: string
+  children?: TreeNode[]
+  key: number | string
+}
+export interface TreeProps {
+  treeData: TreeNode[] | TreeNode
+}
+
+const Tree: React.FC<TreeProps> = ({ treeData }) => {
+  const [activeNode, setActiveNode] = useState<TreeNode["key"]>("")
+  const nodeClick = (key: TreeNode["key"]) => {
+    setActiveNode(key)
+  }
+  const ref = useRef<HTMLDivElement>(null)
+  // useClickAway(() => {
+  //   setActiveNode("")
+  // }, ref)
+  return (
+    <>
+      {((Array.isArray(treeData) && treeData) || [treeData]).map(node => {
+        const active = activeNode === node.key
+        console.log(activeNode)
+        return (
+          <Box ref={ref} key={node.key}>
+            <Button
+              as="div"
+              w="100%"
+              h="2rem"
+              justifyContent="left"
+              color={active ? "white" : "gray.700"}
+              background={active ? "twilight.500" : "transparent"}
+              _hover={{}}
+              _active={{}}
+              transition=""
+              onClick={() => nodeClick(node.key)}
+            >
+              <BsFillCaretRightFill />
+              <Text pl="5px">{node.title}</Text>
+            </Button>
+            {node.children ? (
+              <Box ml="12px">
+                <Tree treeData={node.children}></Tree>
+              </Box>
+            ) : (
+              undefined
+            )}
+          </Box>
+        )
+      })}
+    </>
+  )
 }
 
 export default Tree
--- src/components/tree/stories/Tree.stories.tsx
+++ src/components/tree/stories/Tree.stories.tsx
@@ -1,16 +1,47 @@
 import React from "react"
 import { Story, Meta } from "@storybook/react/types-6-0"
 
-import Tree from "../Tree"
+import Tree, { TreeProps } from "../Tree"
 
 export default {
   title: "Tree",
   component: Tree,
 } as Meta
 
-const Template: Story<{ arg1: boolean }> = args => <Tree {...args}></Tree>
+const Template: Story<TreeProps> = args => <Tree {...args}></Tree>
 
 export const Base = Template.bind({})
 Base.args = {
-  arg1: true,
+  treeData: [
+    {
+      title: "parent 1",
+      key: "0",
+      children: [
+        {
+          title: "parent 1-0",
+          key: "1",
+          children: [
+            {
+              title: "leaf",
+              key: "2",
+            },
+            {
+              title: "leaf",
+              key: "3",
+            },
+          ],
+        },
+        {
+          title: "parent 1-1",
+          key: "4",
+          children: [
+            {
+              title: "leaf",
+              key: "5",
+            },
+          ],
+        },
+      ],
+    },
+  ],
 }
