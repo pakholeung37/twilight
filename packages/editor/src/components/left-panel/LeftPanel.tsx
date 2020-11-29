ed6ae4a5a4e31cce87e1d92a9a2e6ec5fff556f1
Pakho Leung <pakholeung37@gmail.com>
2020-11-29 18:36:03 +0800

    up

--- packages/editor/src/components/left-panel/LeftPanel.tsx
+++ packages/editor/src/components/left-panel/LeftPanel.tsx
@@ -1,5 +1,15 @@
 import React, { useState } from "react"
-import { Box, Flex, List, ListItem, IconButton, Text } from "@chakra-ui/react"
+import {
+  Box,
+  Flex,
+  TabList,
+  TabPanels,
+  TabPanel,
+  Tab,
+  Tabs,
+  IconButton,
+  Text,
+} from "@chakra-ui/react"
 import { RiPaletteFill, RiPaintBrushFill } from "react-icons/ri"
 import { BiText } from "react-icons/bi"
 import { MdInsertPhoto } from "react-icons/md"
@@ -10,100 +20,92 @@ import BackgroundPanel from "./background-panel"
 import SketchPanel from "./sketch-panel"
 
 const menuItems = [
-  { icon: <RiPaletteFill />, name: "调色盘", tab: <PalettePanel key={0} /> },
-  { icon: <BiText />, name: "文本", tab: <TextPanel key={1} /> },
-  { icon: <MdInsertPhoto />, name: "标志", tab: <FigurePanel key={2} /> },
-  {
-    icon: <RiPaintBrushFill />,
-    name: "背景",
-    tab: <BackgroundPanel key={3} />,
-  },
-  { icon: <RiPaintBrushFill />, name: "画板", tab: <SketchPanel key={4} /> },
+  // { icon: <RiPaletteFill />, name: "调色盘", tab: <PalettePanel key={0} /> },
+  { icon: <RiPaintBrushFill />, name: "画板", tab: <SketchPanel /> },
+  // { icon: <BiText />, name: "文本", tab: <TextPanel key={1} /> },
+  { icon: <MdInsertPhoto />, name: "素材", tab: <FigurePanel /> },
+  // {
+  //   icon: <RiPaintBrushFill />,
+  //   name: "背景",
+  //   tab: <BackgroundPanel key={3} />,
+  // },
 ]
 
-const Tabs: React.FC = ({ children }) => {
-  return (
-    <Flex as="nav" justify="center">
-      {children}
-    </Flex>
-  )
-}
-
-const MenuItem: React.FC<{ icon: React.ReactElement; active?: boolean }> = ({
-  icon,
-  children,
-  active,
-}) => {
-  return (
-    <Flex
-      as="div"
-      cursor="pointer"
-      flexDirection="column"
-      justify="center"
-      align="center"
-    >
-      <IconButton
-        as="div"
-        icon={icon}
-        aria-label="menu-button"
-        variant="outline"
-        fontSize="sm"
-        size="sm"
-        isRound
-        color={active ? "white" : "gray.300"}
-        bg={active ? "twilight.500" : undefined}
-        borderColor={active ? "twilight.500" : undefined}
-        _hover={{
-          color: active ? "white" : "twilight.500",
-          borderColor: "twilight.500",
-        }}
-      ></IconButton>
-      <Text as="label" fontSize="xs" my="1" cursor="pointer">
-        {children}
-      </Text>
-    </Flex>
-  )
-}
+// const MenuItem: React.FC<{ icon: React.ReactElement; active?: boolean }> = ({
+//   icon,
+//   children,
+//   active,
+// }) => {
+//   return (
+//     <Flex
+//       as="div"
+//       cursor="pointer"
+//       flexDirection="column"
+//       justify="center"
+//       align="center"
+//     >
+//       <IconButton
+//         as="div"
+//         icon={icon}
+//         aria-label="menu-button"
+//         variant="outline"
+//         fontSize="sm"
+//         size="sm"
+//         isRound
+//         color={active ? "white" : "gray.300"}
+//         bg={active ? "twilight.500" : undefined}
+//         borderColor={active ? "twilight.500" : undefined}
+//         _hover={{
+//           color: active ? "white" : "twilight.500",
+//           borderColor: "twilight.500",
+//         }}
+//       ></IconButton>
+//       <Text as="label" fontSize="xs" my="1" cursor="pointer">
+//         {children}
+//       </Text>
+//     </Flex>
+//   )
+// }
 
 const LeftPanel: React.FC = () => {
   // setting initial tab state
-  const [activeIndex, setActiveIndex] = useState(4)
+  const [activeIndex, setActiveIndex] = useState(0)
   const toggleActiveIndex = (index: number) => {
     setActiveIndex(index)
   }
   return (
-    <Flex
-      justify="center"
-      as="aside"
-      borderRight="1px"
-      h="100%"
-      bg="white"
-      w="320px"
-    >
-      <Box h="100%" w="52px" flexShrink={0} borderRight="1px" py="4">
-        <Tabs>
-          <List px="0" spacing="2">
-            {menuItems.map((item, index) => (
-              <ListItem key={index} onClick={() => toggleActiveIndex(index)}>
-                <MenuItem icon={item.icon} active={activeIndex === index}>
-                  {item.name}
-                </MenuItem>
-              </ListItem>
-            ))}
-          </List>
-        </Tabs>
-      </Box>
-      {menuItems.map((item, index) => (
-        <Box
-          h="100%"
-          display={index === activeIndex ? "block" : "none"}
-          flexGrow={1}
-          key={index}
-        >
-          {item.tab}
-        </Box>
-      ))}
-    </Flex>
+    <Box as="aside" borderRight="1px" h="100%" bg="white" w="272px">
+      <Tabs
+        variant="line"
+        colorScheme="twilight"
+        borderBottom="none"
+        size="sm"
+        index={activeIndex}
+        onChange={toggleActiveIndex}
+      >
+        <TabList>
+          {menuItems.map((item, index) => (
+            <Tab
+              key={index}
+              fontSize="xs"
+              mx=".4rem"
+              px=".2rem"
+              _focus={{}}
+              borderRadius="0"
+            >
+              {item.name}
+            </Tab>
+          ))}
+        </TabList>
+        <TabPanels>
+          {menuItems.map((item, index) => (
+            <TabPanel h="100%" p={0} key={index}>
+              {item.tab}
+            </TabPanel>
+          ))}
+        </TabPanels>
+      </Tabs>
+    </Box>
   )
 }
 
