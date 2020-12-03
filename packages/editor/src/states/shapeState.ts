5998e509c58943eff3820428769d93dc482a3f30
Pakho Leung <pakholeung37@gmail.com>
2020-12-04 00:26:57 +0800

    优化边界性能

--- packages/editor/src/states/shapeState.ts
+++ packages/editor/src/states/shapeState.ts
@@ -1,4 +1,5 @@
-import { throttle } from "lodash"
+import { throttle, memoize } from "lodash"
+import { memo } from "react"
 import { atom, GetRecoilValue, selector, selectorFamily } from "recoil"
 import { ShapeState } from "./ShapeFactory"
 import { shapeManager } from "./ShapeManager"
@@ -30,20 +31,34 @@ export const shapeSelector = selector<ShapeState[]>({
     return shapes
   }
 })
-let count = 0
 
+/**
+ * recoil在极端computed selector时因为每次返回不一样的reference导致没意义的更新
+ * 使得只获取a part of properties of atom不可能.因为atom change始终会触发selector get函数
+ * 而get函数返回一个新的对象, 并不能简单通过reference equal check阻止re-render
+ *
+ * 现通过一个极端的memorize函数先缓解该问题
+ *
+ * https://github.com/facebookexperimental/Recoil/issues/314
+ *
+ */
+const JSONMemorize = memoize((data: string) => ({
+  ...JSON.parse(data)
+}))
 export const shapeTreeNeedSelector = selectorFamily<{ title: string, active: boolean, key: string }, number>({
   key: "shape-tree-need",
   get: (id) => ({ get }) => {
     const shape = get(shapeManager.get(id))
     const selectedShapeId = get(selectedShapeIdAtom)
-    return {
+    const data = {
       title: shape.name,
       active: !!shape.id && (+shape.id === selectedShapeId),
       key: shape.id as string
     }
+    return JSONMemorize(JSON.stringify(data))
   }
 })
+
 // const throttleGet = throttle((get: GetRecoilValue) => {
 //   console.log("recreate: "+ count)
 //   const shapes = get(shapeSelector)
@@ -61,24 +76,10 @@ export const shapeTreeNeedSelector = selectorFamily<{ title: string, active: boo
 export const shapeTreeviewSelector = selector({
   key: "shapes-treeview",
   get: ({ get }) => {
-    console.log("recreate: "+ count)
-    // return throttleGet(get) || { treeData: [] }
-
-    // const shapes = get(shapeSelector)
-    // const selectedShape = get(selectedShapeSelector)
-
-    // return {
-    //   treeData: shapes.map((shape, index) =>({
-    //     title: shape.label,
-    //     active: selectedShape === shape,
-    //     key: index
-    //   }))
-    // }
-
-    const shapesId = get(shapeIdsAtom)
-    const treeData = shapesId.map((id) => get(shapeTreeNeedSelector(id)))
-    return {
-      treeData
-    }
+    const shapesIds = get(shapeIdsAtom)
+    const treeData = shapesIds.map((id) => get(shapeTreeNeedSelector(id)))
+    console.log("recreate: "+ shapesIds)
+    return JSONMemorize(JSON.stringify({ treeData }))
   }
 })
+
