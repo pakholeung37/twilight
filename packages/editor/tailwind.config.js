e678c3d597b43fdd68eeff972d0402032357fc83
Pakho Leung <pakholeung37@gmail.com>
2020-12-17 10:13:40 +0800

    优化tailwind规则， 取消开发环境purge，应该能带来编译速度提升

--- packages/editor/tailwind.config.js
+++ packages/editor/tailwind.config.js
@@ -1,5 +1,6 @@
 module.exports = {
-  purge: ["./src/**/*.{js,jsx,ts,tsx}", "./public/index.html"],
+  // only purge in production, should make dev compile quicker
+  purge: process.env.NODE_ENV === "production" ? ["./src/**/*.{js,jsx,ts,tsx}", "./public/index.html"] : [],
   darkMode: false, // or 'media' or 'class'
   theme: {
     extend: {},
