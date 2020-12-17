b3fc26684368aebcc0f87fecd4566fba4aadaf0e
Pakho Leung <pakholeung37@gmail.com>
2020-12-17 20:38:24 +0800

    go home write code

--- packages/editor/src/components/workspace/snap-system/SnapSystem.tsx
+++ packages/editor/src/components/workspace/snap-system/SnapSystem.tsx
@@ -1,12 +1,59 @@
 import React from "react"
-import { useRecoilValue } from "recoil"
 // TODO 这个依赖需要去掉
 import Konva from "konva"
 
+type GuideLine = { start?: number; end?: number; position: number }
 export class SnapSystemManager {
-  constructor() {}
-  addNode(node: Konva.Node) {}
-  removeNode(node: Konva.Node) {}
+  private shapeSet: Set<Konva.Node> = new Set()
+  private guideLineH: GuideLine[] = []
+  private guideLineV: GuideLine[] = []
+  public constructor() {}
+  protected addNode(node: Konva.Node) {
+    this.shapeSet.add(node)
+  }
+  protected removeNode(node: Konva.Node) {
+    if (this.shapeSet.has(node)) {
+      this.shapeSet.delete(node)
+    }
+  }
+
+  protected freeze() {
+    this.shapeSet.forEach(shape => {
+      const box = shape.getClientRect()
+      this.guideLineV.push(
+        ...[
+          { start: box.y, end: box.y + box.height, position: box.x },
+          {
+            start: box.y,
+            end: box.y + box.height,
+            position: box.x + box.width / 2,
+          },
+          {
+            start: box.y,
+            end: box.y + box.height,
+            position: box.x + box.width,
+          },
+        ],
+      )
+      this.guideLineH.push(
+        ...[
+          { start: box.x, end: box.x + box.width, position: box.y },
+          {
+            start: box.x,
+            end: box.x + box.width,
+            position: box.y + box.height / 2,
+          },
+          {
+            start: box.x,
+            end: box.x + box.width,
+            position: box.y + box.height,
+          },
+        ],
+      )
+    })
+    this.guideLineH.sort((a, b) => a.position - b.position)
+    this.guideLineV.sort((a, b) => a.position - b.position)
+  }
 }
 
 export const snapSystemManager = new SnapSystemManager()
